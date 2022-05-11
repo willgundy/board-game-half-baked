4f80cbb06443d86ddf0521f9b58a23605f1b9817
Will Gundy <willgunderson503@gmail.com>
2022-05-11 15:14:51 -0700

    changed to Sign In and Sign Up forms being separate

--- src/AuthPage.js
+++ src/AuthPage.js
@@ -10,34 +10,49 @@ export default function AuthPage({ setUser }) {
     e.preventDefault();
       
     // sign the user in using the form state
-
+    const user = await signIn(email, password);
     // set the user in App.js state using the correct prop callback. If you did the ternary right in App.js, this should automatically redirect the user to the board game list
+    setUser(user);
   }
     
-  async function handleSignUp() {
+  async function handleSignUp(e) {
+    e.preventDefault();
     // sign the user up using the form state
-
+    const user = await signUp(email, password);
     // set the user in App.js state using the correct prop callback. If you did the ternary right in App.js, this should automatically redirect the user to the board game list
+    setUser(user);
   }
 
   return (
     <div className='auth'>
       <h1><em>Boardzo</em></h1>
+      {/* on submit, sign the user up using the function defined above */}
+      <form>
+        <label>
+            Email
+          {/* on change, update the form state for email */}
+          <input required type="email" name="email" />
+        </label>
+        <label>
+            Password
+          {/* on change, update the form state for password */}
+          <input required type="password" name="password" />
+        </label>
+        <button>Sign Up</button>
+      </form>
       {/* on submit, sign the user in using the function defined above */}
       <form>
         <label>
             Email
           {/* on change, update the form state for email */}
-          <input required type="email" name="email" onChange={e => setEmail(e.target.value)} />
+          <input required type="email" name="email" />
         </label>
         <label>
             Password
           {/* on change, update the form state for password */}
-          <input required type="password" name="password" onChange={e => setPassword(e.target.value)}/>
+          <input required type="password" name="password" />
         </label>
         <button>Sign In</button>
-        {/* on clicking sign up, sign the user up using the function defined above */}
-        <button type="button" >Sign Up</button>
       </form>
     </div>
   );
