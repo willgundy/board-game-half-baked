7325c727adaf7a82064df432b96db31ea6840c00
Will Gundy <willgunderson503@gmail.com>
2022-05-11 15:28:56 -0700

    finished up ListPage file

--- src/ListPage.js
+++ src/ListPage.js
@@ -4,11 +4,26 @@ import Game from './Game';
 
 export default function ListPage() {
   // you'll need some state to hold onto the array of games
+  const [games, setGames] = useState([]);
 
   // fetch the games on load and inject them into state
+  useEffect(() => {
+    async function getGameInfo() {
+      const games = await getGames();
+
+      setGames(games);
+    }
+    getGameInfo();
+  });
   return (
     <div className='list games'>
       {/* map through the games in state and render Game components */}
+      {games.map((game, i) => 
+        <Game 
+          key={i}
+          game={game}
+        />
+      )}
     </div>
   );
 }
