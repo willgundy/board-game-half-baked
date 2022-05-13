fea1379d4ad180557de73918f4ed6235a155fe79
Will Gundy <willgunderson503@gmail.com>
2022-05-12 19:42:43 -0700

    return auth page to the functional setup

--- src/AuthPage.js
+++ src/AuthPage.js
@@ -1,69 +1,59 @@
-import React from 'react';
-import { withRouter } from 'react-router-dom';
+import { useState } from 'react';
 import { signIn, signUp } from './services/fetch-utils.js';
 
-export default withRouter(class AuthPage extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      email: '',
-      password: ''
-    };
-  }
+export default function AuthPage({ setUser }) {
+  // you'll need to track the form state of the email and password
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
-  async handleSignIn(e) {
+  async function handleSignIn(e) {
     e.preventDefault();
-
-    console.log(this.state.email, this.state.password);
       
     // sign the user in using the form state
-    const user = await signIn(this.state.email, this.state.password);
+    const user = await signIn(email, password);
     // set the user in App.js state using the correct prop callback. If you did the ternary right in App.js, this should automatically redirect the user to the board game list
-    this.setState({ email: user.email, password: user.password });
+    setUser(user);
   }
     
-  async handleSignUp(e) {
+  async function handleSignUp(e) {
     e.preventDefault();
     // sign the user up using the form state
-    const user = await signUp(this.state.email, this.state.password);
+    const user = await signUp(email, password);
     // set the user in App.js state using the correct prop callback. If you did the ternary right in App.js, this should automatically redirect the user to the board game list
-    this.setState({ email: user.email, password: user.password });
+    setUser(user);
   }
 
-  render() {
-    return (
-      <div className='auth'>
-        <h1><em>Boardzo</em></h1>
-        {/* on submit, sign the user up using the function defined above */}
-        <form onSubmit={this.handleSignUp}>
-          <label>
-              Email
-            {/* on change, update the form state for email */}
-            <input required type="email" name="email" onChange={e => this.setState({ email: e.target.value })}/>
-          </label>
-          <label>
-              Password
-            {/* on change, update the form state for password */}
-            <input required type="password" name="password" onChange={e => this.setState({ password : e.target.value })}/>
-          </label>
-          <button>Sign Up</button>
-        </form>
-        {/* on submit, sign the user in using the function defined above */}
-        <form onSubmit={this.handleSignIn}>
-          <label>
-              Email
-            {/* on change, update the form state for email */}
-            <input required type="email" name="email" onChange={e => this.setState({ email: e.target.value })}/>
-          </label>
-          <label>
-              Password
-            {/* on change, update the form state for password */}
-            <input required type="password" name="password" onChange={e => this.setState({ password : e.target.value })}/>
-          </label>
-          <button>Sign In</button>
-        </form>
-      </div>
-    );
-  }
+  return (
+    <div className='auth'>
+      <h1><em>Boardzo</em></h1>
+      {/* on submit, sign the user up using the function defined above */}
+      <form onSubmit={handleSignUp}>
+        <label>
+            Email
+          {/* on change, update the form state for email */}
+          <input required type="email" name="email" onChange={e => setEmail(e.target.value)}/>
+        </label>
+        <label>
+            Password
+          {/* on change, update the form state for password */}
+          <input required type="password" name="password" onChange={e => setPassword(e.target.value)}/>
+        </label>
+        <button>Sign Up</button>
+      </form>
+      {/* on submit, sign the user in using the function defined above */}
+      <form onSubmit={handleSignIn}>
+        <label>
+            Email
+          {/* on change, update the form state for email */}
+          <input required type="email" name="email" onChange={e => setEmail(e.target.value)}/>
+        </label>
+        <label>
+            Password
+          {/* on change, update the form state for password */}
+          <input required type="password" name="password" onChange={e => setPassword(e.target.value)}/>
+        </label>
+        <button>Sign In</button>
+      </form>
+    </div>
+  );
 }
-);
