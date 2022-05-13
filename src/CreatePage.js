86ee0470dd9b21a1109b79f9f027acc2991bbb58
Will Gundy <willgunderson503@gmail.com>
2022-05-12 19:08:20 -0700

    testing component refactoring

--- src/CreatePage.js
+++ src/CreatePage.js
@@ -1,83 +1,78 @@
-import { useState } from 'react';
-import { useHistory } from 'react-router-dom';
+import React from 'react';
 import { createGame } from './services/fetch-utils';
 
-export default function CreatePage() {
-  // you'll need the history hook from react-router-dom to do your redirecting in the handleSubmit
-  const history = useHistory();
-
-  // here's the state you'll need:
-    // title;
-    // genre;
-    // designer;
-    // description;
-    // minPlayers;
-    // maxPlayers;
-  const [gameInForm, setGameInForm] = useState({
-    title: '',
-    genre: '',
-    designer: '',
-    description: '',
-    min_players: 2,
-    max_players: 10
-  });
-
-  async function handleSubmit(e) {
-    e.preventDefault();
-
-    // create a game
-    await createGame(gameInForm);
-    // use history.push to send the user to the list page
-    history.push('/board-games');
+export default class CreatePage extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      title: '',
+      genre: '',
+      designer: '',
+      description: '',
+      min_players: 2,
+      max_players: 10
+    };
   }
 
-  return (
-    <div className='create'>
-      {/* on submit, call your handleSubmit function */}
-      <form onSubmit={handleSubmit}>
-        <h2>Add board game</h2>
-        <label>
-            Title
-          {/* on change, set the title in state */}
-          <input required name='title' onChange={(e) => setGameInForm({ ...gameInForm, title: e.target.value })} />
-        </label>
-        <label>
-            Genre
-          {/* on change, set the genre in state */}
-          <select required
-            onChange={(e) => setGameInForm({ ...gameInForm, genre: e.target.value })}
-          >
-            <option>Tile-laying</option>
-            <option>Economic</option>
-            <option>War</option>
-            <option>Card</option>
-            <option>Abstract</option>
-            <option>Cooperative</option>
-            <option>Solo</option>
-          </select>
-        </label>
-        <label>
-            Designer
-          {/* on change, set the designer in state */}
-          <input required name='designer' onChange={(e) => setGameInForm({ ...gameInForm, designer: e.target.value })}/>
-        </label>
-        <label>
-            Min Players
-          {/* on change, set the min players in state */}
-          <input required name='min_players' onChange={(e) => setGameInForm({ ...gameInForm, min_players: e.target.value })}/>
-        </label>
-        <label>
-            Max Players
-          {/* on change, set the max players in state */}
-          <input required name='max_players' onChange={(e) => setGameInForm({ ...gameInForm, max_players: e.target.value })}/>
-        </label>
-        <label>
-            Description
-          {/* on change, set the description in state */}
-          <textarea required name='description' onChange={(e) => setGameInForm({ ...gameInForm, description: e.target.value })} />
-        </label>
-        <button>Create game</button>
-      </form>
-    </div>
-  );
+
+  async handleSubmit(e) {
+    e.preventDefault();
+
+    await createGame(this.state);
+
+    this.props.history.push('/board-games');
+  }
+
+  render() {
+
+    return (
+      <div className='create'>
+        {/* on submit, call your handleSubmit function */}
+        <form onSubmit={this.handleSubmit}>
+          <h2>Add board game</h2>
+          <label>
+              Title
+            {/* on change, set the title in state */}
+            <input required name='title' onChange={(e) => this.setState({ title: e.target.value })} />
+          </label>
+          <label>
+              Genre
+            {/* on change, set the genre in state */}
+            <select required
+              onChange={(e) => this.setState({ genre: e.target.value })}
+            >
+              <option>Tile-laying</option>
+              <option>Economic</option>
+              <option>War</option>
+              <option>Card</option>
+              <option>Abstract</option>
+              <option>Cooperative</option>
+              <option>Solo</option>
+            </select>
+          </label>
+          <label>
+              Designer
+            {/* on change, set the designer in state */}
+            <input required name='designer' onChange={(e) => this.setState({ designer: e.target.value })}/>
+          </label>
+          <label>
+              Min Players
+            {/* on change, set the min players in state */}
+            <input required name='min_players' onChange={(e) => this.setState({ min_players: e.target.value })}/>
+          </label>
+          <label>
+              Max Players
+            {/* on change, set the max players in state */}
+            <input required name='max_players' onChange={(e) => this.setState({ max_players: e.target.value })}/>
+          </label>
+          <label>
+              Description
+            {/* on change, set the description in state */}
+            <textarea required name='description' onChange={(e) => this.setState({ description: e.target.value })} />
+          </label>
+          <button>Create game</button>
+        </form>
+      </div>
+    );
+  }
 }
--- src/ListPage.js
+++ src/ListPage.js
@@ -1,30 +1,31 @@
-import { useState, useEffect } from 'react';
+import React from 'react';
 import { getGames } from './services/fetch-utils';
 import Game from './Game';
+export default class ListPage extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      games: []
+    };
+  }
 
-export default function ListPage() {
-  // you'll need some state to hold onto the array of games
-  const [games, setGames] = useState([]);
+  async componentDidMount() {
+    const gameInfo = await getGames();
 
-  // fetch the games on load and inject them into state
-  useEffect(() => {
-    async function getGameInfo() {
-      const gameInfo = await getGames();
+    this.setState({ games: gameInfo });
+  }
 
-      setGames(gameInfo);
-    }
-    getGameInfo();
-  }, []);
-
-  return (
-    <div className='list games'>
-      {/* map through the games in state and render Game components */}
-      {games.map((game, i) => 
-        <Game 
-          key={game + i}
-          game={game}
-        />
-      )}
-    </div>
-  );
+  render() {
+    return (
+      <div className='list games'>
+        {/* map through the games in state and render Game components */}
+        {this.state.games.map((game, i) => 
+          <Game 
+            key={game + i}
+            game={game}
+          />
+        )}
+      </div>
+    );
+  }
 }
